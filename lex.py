a47b201969c0993c3be7f02b5e5b2226990ffe51
DymashevskijSP <sp.dymashevskij@gmail.com>
2021-09-18 18:18:37 +0300

    lexer with test

--- lex.py
+++ lex.py
@@ -2,49 +2,65 @@ import ply.lex as lex
 import sys
 
 reserved = {
-  'if': 'IF',
-  'then': 'THEN',
-  'else': 'ELSE'
+    'Terminal': 'TERMINAL',
+    'Sigma': 'SIGMA',
+    'Start': 'START',
+    'Q': 'STATES',
+    'Delta': 'DELTA'
 }
 
 tokens = [
-  'NUM',
-  'PLUS',
-  'MULT',
-  'ID'
-] + list(reserved.values())
+             'NUM',
+             'ARRFUNC',
+             'SEP',
+             'UNEXPECTED_ID',
+             'OPEN_BR',
+             'CLOSED_BR'
+         ] + list(reserved.values())
 
 
 def t_ID(t):
-  r'[a-z_][a-z_0-9]*'
-  t.type = reserved.get(t.value, 'ID')
-  return t
+    r'[A-Z][a-z_0-9]*'
+    t.type = reserved.get(t.value, 'UNEXPECTED_ID')
+    return t
 
 
 def t_NUM(t):
-  r'[0-9]+'
-  t.value = int(t.value)
-  return t
+    r'([01]{8})+'
+    chars = ['0b' + t.value[i:i + 8] for i in range(0, len(t.value) - 7, 8)]
+    value = ""
+    for i in chars:
+        value += chr(int(i, 2))
+    t.value = value
+    return t
 
-t_PLUS = r'\+'
-t_MULT = r'\*'
 
+t_ARRFUNC = r'->'
+t_SEP = r','
+t_OPEN_BR = r'\('
+t_CLOSED_BR = r'\)'
 t_ignore = ' \t'
 
+
 def t_newline(t):
-  r'\n+'
-  t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
 
 def t_error(t):
-  print("Illegal character '%s'" % t.value[0])
-  t.lexer.skip(1)
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
 
 lexer = lex.lex()
-
-lexer.input(sys.argv[1])
-
+fin = open(sys.argv[1], 'r')
+lexer.input(fin.read())
+fin.close()
+fout = open(sys.argv[1] + '.out', 'w')
 while True:
-  tok = lexer.token()
-  if not tok:
-    break
-  print(tok)
+    tok = lexer.token()
+    if not tok:
+        break
+    fout.write(str(tok))
+    fout.write('\n')
+fout.close()
