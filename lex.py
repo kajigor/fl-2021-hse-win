db4189d2428c9918bdea3a1a47ab2dbb241e4254
timav <t.vasilevskij2002@gmail.com>
2021-09-19 22:15:42 +0300

    initial for HW02

--- lex.py
+++ lex.py
@@ -2,49 +2,56 @@ import ply.lex as lex
 import sys
 
 reserved = {
-  'if': 'IF',
-  'then': 'THEN',
-  'else': 'ELSE'
+    'Alphabet': 'ALPHABET',
+    'Startvertex': 'STARTVERTEX',
+    'Edge': 'EDGE',
+    'States': 'STATES',
+    'Terminalvertexes': 'TERMINALVERTEXES'
 }
 
 tokens = [
-  'NUM',
-  'PLUS',
-  'MULT',
-  'ID'
-] + list(reserved.values())
+             'NUM',
+             'UNEXPECTED_ID',
+         ] + list(reserved.values())
 
 
 def t_ID(t):
-  r'[a-z_][a-z_0-9]*'
-  t.type = reserved.get(t.value, 'ID')
-  return t
+    r'[A-Z][a-z_0-9]*'
+    t.type = reserved.get(t.value, 'UNEXPECTED_ID')
+    return t
 
 
 def t_NUM(t):
-  r'[0-9]+'
-  t.value = int(t.value)
-  return t
+    r'([01]{8})+'
+    c = ['0b' + t.value[i:i + 8] for i in range(0, len(t.value) - 7, 8)]
+    value = ""
+    for i in c:
+        value += chr(int(i, 2))
+    t.value = value
+    return t
 
-t_PLUS = r'\+'
-t_MULT = r'\*'
 
 t_ignore = ' \t'
 
+
 def t_newline(t):
-  r'\n+'
-  t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
 
 def t_error(t):
-  print("Illegal character '%s'" % t.value[0])
-  t.lexer.skip(1)
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
 
 lexer = lex.lex()
-
-lexer.input(sys.argv[1])
-
-while True:
-  tok = lexer.token()
-  if not tok:
-    break
-  print(tok)
+with open(sys.argv[1], 'r') as file_in:
+    lexer.input(file_in.read())
+    file_out = open(sys.argv[1] + '.out', 'w')
+    while True:
+        token = lexer.token()
+        if not token:
+            break
+        file_out.write(str(token))
+        file_out.write('\n')
+    file_out.close()
