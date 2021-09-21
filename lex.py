d25fc86a66b4d4a2da98e87c083c1393cdd80188
alexluk4723 <71816679+alexluk4723@users.noreply.github.com>
2021-09-21 23:43:18 +0300

    :)

--- lex.py
+++ lex.py
@@ -2,32 +2,37 @@ import ply.lex as lex
 import sys
 
 reserved = {
-  'if': 'IF',
-  'then': 'THEN',
-  'else': 'ELSE'
+  '-->': 'EDGE',
+  'state': 'STATE',
+  'begin': 'BEGIN',
+  'true': 'BOOL',
+  'false': 'BOOL'
 }
 
 tokens = [
   'NUM',
-  'PLUS',
-  'MULT',
-  'ID'
+  'WORD',
+  'EDGE',
+  'BOOL'
 ] + list(reserved.values())
 
 
-def t_ID(t):
-  r'[a-z_][a-z_0-9]*'
-  t.type = reserved.get(t.value, 'ID')
+def t_WORD(t):
+  r'(begin)|(state)'
+  t.type = reserved.get(t.value, 'WORD')
   return t
 
+def t_BOOL(t):
+  r'(true)|(false)'
+  t.type = reserved.get(t.value, 'WORD')
+  return t
 
 def t_NUM(t):
   r'[0-9]+'
   t.value = int(t.value)
   return t
 
-t_PLUS = r'\+'
-t_MULT = r'\*'
+t_EDGE = r'-->'
 
 t_ignore = ' \t'
 
@@ -41,7 +46,8 @@ def t_error(t):
 
 lexer = lex.lex()
 
-lexer.input(sys.argv[1])
+lexer.input(open(sys.argv[1], 'r').read())
+sys.stdout = open(sys.argv[1] + '.out', 'w')
 
 while True:
   tok = lexer.token()
