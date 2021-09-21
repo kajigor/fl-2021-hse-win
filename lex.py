9cc325d7372787defb6641f82ef0b198324c5477
Emily <emilexxxxx@gmail.com>
2021-09-21 23:01:29 +0300

    flexer

--- lex.py
+++ lex.py
@@ -2,49 +2,68 @@ import ply.lex as lex
 import sys
 
 reserved = {
-  'if': 'IF',
-  'then': 'THEN',
-  'else': 'ELSE'
+    'Sigma': 'SIGMA',
+    'T': 'TERMINAL',
+    'q0': 'START_STATE',
+    'Q': 'STATES',
+    'delta': 'DELTA'
 }
 
 tokens = [
-  'NUM',
-  'PLUS',
-  'MULT',
-  'ID'
-] + list(reserved.values())
+             'NUM',
+             'ID',
+             'COMMA',
+             'COLON',
+             'OPEN_BR',
+             'CLOSED_BR',
+             'DASH'
+         ] + list(reserved.values())
 
 
 def t_ID(t):
-  r'[a-z_][a-z_0-9]*'
-  t.type = reserved.get(t.value, 'ID')
-  return t
+    r'[a-z_][a-z_0-9]*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
 
 
 def t_NUM(t):
-  r'[0-9]+'
-  t.value = int(t.value)
-  return t
+    r'[0-9]+'
+    t.value = int(t.value)
+    return t
 
-t_PLUS = r'\+'
-t_MULT = r'\*'
 
+
+
+t_COMMA = r','
+t_COLON = r':'
+t_OPEN_BR = r'\('
+t_CLOSED_BR = r'\)'
+t_DASH = r'--'
+t_TERMINAL = r'T'
+t_SIGMA = r'Sigma'
+t_START_STATE = r'q0'
+t_STATES = r'Q'
+t_DELTA = r'delta'
 t_ignore = ' \t'
 
+
 def t_newline(t):
-  r'\n+'
-  t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
 
 def t_error(t):
-  print("Illegal character '%s'" % t.value[0])
-  t.lexer.skip(1)
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
 
 lexer = lex.lex()
 
-lexer.input(sys.argv[1])
+lexer.input(open(sys.argv[1], 'r').read())
+sys.stdout = open(sys.argv[1] + '.out', 'w')
 
 while True:
-  tok = lexer.token()
-  if not tok:
-    break
-  print(tok)
+    tok = lexer.token()
+    if not tok:
+        break
+    print(tok)
