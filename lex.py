3be6cf5b64182b07764850ace5e02ee7b2bcbead
slawa4s <suvslavka@gmail.com>
2021-10-01 10:53:47 +0300

    add deleted file

--- lex.py
+++ lex.py
@@ -2,32 +2,40 @@ import ply.lex as lex
 import sys
 
 reserved = {
-  'if': 'IF',
-  'then': 'THEN',
-  'else': 'ELSE'
+  'vertex': 'Start_new_vertex',
+  'edge': 'Start_new_edge',
+  'true': 'Is_Terminal',
+  'false': 'Is_Not_Terminal',
 }
 
 tokens = [
-  'NUM',
-  'PLUS',
-  'MULT',
-  'ID'
+  'VERTEXNAME',
+  'SEMICOLON',
+  'MOVE_TO',
+  'COMMA',
+  'CURLY_BRAKETS',
+  'NUM'
 ] + list(reserved.values())
 
-
-def t_ID(t):
+def t_VERTEXNAME(t):
   r'[a-z_][a-z_0-9]*'
-  t.type = reserved.get(t.value, 'ID')
+  t.type = reserved.get(t.value, 'VERTEXNAME')
   return t
 
+def t_MOVE_TO(t):
+  r'\'.*?\''
+  t.value = t.value[1 : -1]
+  t.type = reserved.get(t.value, 'MOVE_TO')
+  return t
+
+t_SEMICOLON = r'\;'
+t_COMMA = r'\,'
+t_CURLY_BRAKETS = r'\{|\}'
 
 def t_NUM(t):
   r'[0-9]+'
   t.value = int(t.value)
   return t
-
-t_PLUS = r'\+'
-t_MULT = r'\*'
 
 t_ignore = ' \t'
 
@@ -41,7 +49,8 @@ def t_error(t):
 
 lexer = lex.lex()
 
-lexer.input(sys.argv[1])
+lexer.input(open(sys.argv[1], 'r').read())
+sys.stdout = open(sys.argv[1] + '.out', 'w')
 
 while True:
   tok = lexer.token()
