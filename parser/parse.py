c95340a693884b13e862b29baec317536e35dd95
nikolyanikolya <nickigna610@gmail.com>
2021-10-04 11:54:02 +0300

    HW04

--- parser/parse.py
+++ parser/parse.py
@@ -20,7 +20,7 @@ def p_if(p):
 
 def p_expression_plus(p):
   'expression : expression PLUS expression'
-  p[0] = p[1] - p[3]
+  p[0] = p[1] + p[3]
 
 def p_expression_term(p):
   'expression : term'
