aae05938ffb1595c5dd01a408cae1e8f208af911
rmzs <rmzs0711@yandex.ru>
2021-10-04 19:35:06 +0300

    parser

--- parser/lex.py
+++ parser/lex.py
@@ -1,49 +1,116 @@
-import ply.lex as lex 
+# import ply.lex as lex
+# import sys
+
+# reserved = {
+#   'if': 'IF',
+#   'then': 'THEN',
+#   'else': 'ELSE'
+# }
+#
+# tokens = [
+#   'NUM',
+#   'PLUS',
+#   'MULT',
+#   'ID',
+#   'LBR',
+#   'RBR'
+# ] + list(reserved.values())
+#
+# def t_ID(t):
+#   r'[a-z_][a-z_0-9]*'
+#   t.type = reserved.get(t.value, 'ID')
+#   return t
+#
+# def t_NUM(t):
+#   r'[0-9]+'
+#   t.value = int(t.value)
+#   return t
+#
+# t_PLUS = r'\+'
+# t_MULT = r'\*'
+# t_LBR = r'\('
+# t_RBR = r'\)'
+#
+# t_ignore = ' \t'
+#
+# def t_newline(t):
+#   r'\n+'
+#   t.lexer.lineno += len(t.value)
+#
+# def t_error(t):
+#   print("Illegal character '%s'" % t.value[0])
+#   t.lexer.skip(1)
+#
+# lexer = lex.lex()
+
 import sys
+import ply.lex as lex
 
 reserved = {
-  'if': 'IF', 
-  'then': 'THEN',
-  'else': 'ELSE'
+    'States': 'STATES',
+    'Start': 'START',
+    'End': 'END',
+    'Alphabet': 'ALPHABET',
+    'Function': 'FUNCTION'
 }
 
 tokens = [
-  'NUM', 
-  'PLUS', 
-  'MULT',
-  'ID',
-  'LBR',
-  'RBR'
-] + list(reserved.values())
+             # 'COMMENT',
+             'TRANSFER',
+             'STATE',
+             'EQUAL',
+             'BRACKET',
+             'SEMICOLON',
+         ] + list(reserved.values())
 
-def t_ID(t):
-  r'[a-z_][a-z_0-9]*'
-  t.type = reserved.get(t.value, 'ID')
-  return t
+t_SEMICOLON = r';'
+t_BRACKET = r'({|})'
+t_EQUAL = r'='
+t_ignore = ' '
 
-def t_NUM(t): 
-  r'[0-9]+'
-  t.value = int(t.value)
-  return t
 
-t_PLUS = r'\+'
-t_MULT = r'\*'
-t_LBR = r'\('
-t_RBR = r'\)'
+# def t_COMMENT(t):
+#     r'//.*\n'
+#     t.value = t.value[2:]
+#     return t
 
-t_ignore = ' \t'
 
-def t_newline(t): 
-  r'\n+'
-  t.lexer.lineno += len(t.value)
+def t_STATE(t):
+    r'[^\s={};"/]+(,)?(\s)?'
+    t.value = t.value[:-1]
+    if (t.value[-1] == ','):
+        t.value = t.value[:-1]
+    t.type = reserved.get(t.value, 'STATE')
+    return t
 
-def t_error(t): 
-  print("Illegal character '%s'" % t.value[0])
-  t.lexer.skip(1)
 
-lexer = lex.lex() 
+def t_TRANSFER(t):
+    r'"([^"\\]|\\(")?)+"(,)?\s'
+    t.value = t.value[:-1]
+    if (t.value[-1] == ','):
+        t.value = t.value[:-1]
+    return t
 
-#lexer.input(sys.argv[1]) 
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+# lexer.input(open(sys.argv[1], 'r').read())
+
+# while True:
+#     tok = lexer.token()
+#     if not tok:
+#         break
+#     print(tok)
+
+# lexer.input(sys.argv[1])
 
 #while True: 
 #  r'\+' 
--- parser/parse.py
+++ parser/parse.py
@@ -1,64 +1,156 @@
+import sys
+
 import ply.yacc as yacc
 
 from lex import tokens
 
+
 # if 42 then (if 0 then 777 else 9)
 
 
-def p_if(p):
-  '''if : IF expression THEN if ELSE if
-        | IF expression THEN if
-        | expression '''
-  if len(p) == 7:
-    p[0] = p[4] if p[2] == 0 else p[6]
-  else:
-    if len(p) == 5:
-      if p[2] == 0 :
-        p[0] = p[4]
-    else:
-      p[0] = p[1]
+# def p_if(p):
+#   '''if : IF expression THEN if ELSE if
+#         | IF expression THEN if
+#         | expression '''
+#   if len(p) == 7:
+#     p[0] = p[4] if p[2] == 0 else p[6]
+#   else:
+#     if len(p) == 5:
+#       if p[2] == 0 :
+#         p[0] = p[4]
+#     else:
+#       p[0] = p[1]
+#
+# def p_expression_plus(p):
+#   'expression : expression PLUS expression'
+#   p[0] = p[1] - p[3]
+#
+# def p_expression_term(p):
+#   'expression : term'
+#   p[0] = p[1]
+# #
+# def p_term_times(p):
+#   'term : term MULT factor'
+#   p[0] = p[1] * p[3]
+# #
+# def p_term_factor(p):
+#   'term : factor'
+#   p[0] = p[1]
+#
+# def p_factor_num(p):
+#   'factor : NUM'
+#   p[0] = p[1]
+#
+# def p_factor_expr(p):
+#   'factor : LBR expression RBR'
+#   p[0] = p[2]
 
-def p_expression_plus(p):
-  'expression : expression PLUS expression'
-  p[0] = p[1] - p[3]
+class Automate:
+    def parts_str(self):
+        st = []
+        for part in self.parts:
+            st.append(str(part))
+        return "\n".join(st)
 
-def p_expression_term(p):
-  'expression : term'
-  p[0] = p[1]
+    def __repr__(self):
+        return self.type + ":\n\t" + self.parts_str().replace("\n", "\n\t")
 
-def p_term_times(p):
-  'term : term MULT factor'
-  p[0] = p[1] * p[3]
+    def add_parts(self, parts):
+        self.parts += parts
+        return self
 
-def p_term_factor(p):
-  'term : factor'
-  p[0] = p[1]
+    def __init__(self, type, parts):
+        self.type = type
+        self.parts = parts
 
-def p_factor_num(p):
-  'factor : NUM'
-  p[0] = p[1]
+def p_automate(p):
+    '''automate : states start end alphabet function'''
+    p[0] = Automate("AUTOMATE", p[1:])
 
-def p_factor_expr(p):
-  'factor : LBR expression RBR'
-  p[0] = p[2]
+def p_states_definition(p):
+    'states_definition : EQUAL BRACKET state_list BRACKET SEMICOLON '
+    p[0] = p[3]
+
+def p_states(p):
+    'states : STATES states_definition'
+    p[0] = Automate("STATES", p[2])
+
+def p_start(p):
+    'start : START states_definition'
+    p[0] = Automate("START", p[2])
+
+def p_end(p):
+    'end : END states_definition'
+    p[0] = Automate("END", p[2])
 
 def p_error(p):
-  print("Syntax error")
+    print("Syntax error")
+
+
+def p_state_list(p):
+    '''state_list : state state_list
+                  | state'''
+    p[0] = []
+    p[0] += [p[1]]
+    if (len(p) == 3):
+        p[0] += p[2]
+
+def p_state(p):
+    'state : STATE'
+    p[0] = p[1]
+
+
+def p_transfer(p):
+    'transfer : TRANSFER'
+    p[0] = p[1]
+
+
+def p_transfer_list(p):
+    '''transfer_list : transfer transfer_list
+                     | transfer'''
+    p[0] = []
+    p[0] += [p[1]]
+    if (len(p) == 3):
+        p[0] += p[2]
+
+def p_alphabet_definition(p):
+    'alphabet_definition : EQUAL BRACKET transfer_list BRACKET SEMICOLON'
+    p[0] = p[3]
+
+def p_alphabet(p):
+    'alphabet : ALPHABET alphabet_definition'
+    p[0] = Automate("ALPHABET", p[2])
+
+def p_function(p):
+    'function : FUNCTION function_def'
+    p[0] = Automate("FUNCTION", p[2])
+
+def p_function_def(p):
+    'function_def : EQUAL BRACKET function_list BRACKET SEMICOLON'
+    p[0] = p[3]
+
+def p_where(p):
+    'where : state transfer state'
+    p[0] = p[1] + " --(" + p[2] + ")--> " + p[3]
+
+def p_function_list(p):
+    '''function_list : where function_list
+                     | where'''
+    p[0] = []
+    p[0] += [p[1]]
+    if (len(p) == 3):
+        p[0] += p[2]
+
+sys.stdout = open(sys.argv[1] + '.out', 'w')
 
 parser = yacc.yacc()
-
-while True:
-  try:
-    s = input("calc> ")
-  except EOFError:
-    break
-  if not s:
-    continue
-  result=parser.parse(s)
-  print(result)
-
-
-
-
-
-
+s = open(sys.argv[1], 'r').read()
+# while True:
+# try:
+#   s = input("calc> ")
+# except EOFError:
+#   break
+# if not s:
+# continue
+result = parser.parse(s)
+print(result)
