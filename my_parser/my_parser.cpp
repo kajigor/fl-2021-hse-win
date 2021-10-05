6740c1acbe2f9cdbdafd597b1cc029f453869736
Egor Kuragin <egor.kuragin@gmail.com>
2021-10-06 00:04:04 +0300

    Final update

--- my_parser/my_parser.cpp
+++ my_parser/my_parser.cpp
@@ -13,9 +13,15 @@ private:
 public:
   explicit Symbol() = default;
   void read_word(std::string s) { word = s.substr(1, s.size() - 2); }
-  const std::string& get_word() const {
+  std::string& get_word(){
       return word;
   };
+  const std::string& get_word() const{
+      return word;
+  };
+  bool operator==(const Symbol s) const {
+    return get_word() == s.get_word();
+  }
 };
 
 struct Transition {
@@ -79,6 +85,22 @@ public:
     }
   }
 
+  int cond_size() {
+    return conditions.size();
+  }
+
+  int get_begin() {
+    return begin_state;
+  }
+
+  int get_end() {
+    return end_state;
+  }
+
+  const std::vector<Symbol> get_cond() {
+    return conditions;
+  }
+
   void print(std::ofstream &fout) const {
     fout << begin_state << " -> " << end_state << " by symbols: ";
     for (const auto& sy : conditions) {
@@ -91,7 +113,6 @@ public:
 struct Alphabet {
 private:
   std::vector<Symbol> alph;
-
 public:
   void read_alph(std::string &s) {
     int i = 1;
@@ -127,12 +148,38 @@ public:
       fout << '\"' << sy.get_word() << "\" ";
     }
   }
+
+  int alph_size() {
+    return alph.size();
+  }
+
+  std::vector<Symbol>& get_alph() {
+    return alph;
+  }
+
+  void test_alph(std::ofstream &fout) {
+    if (alph.empty()) {
+      fout << "Alphabet is empty";
+      return;
+    }
+    for (int i = 0; i < alph.size(); i++) {
+      for (int j = i + 1; j < alph.size(); j++) {
+        if (alph[i] == alph[j]) {
+          fout << "Alphabet has non-unique symbols\n";
+          return;
+        }
+      }
+    }
+    fout << "Alphabet consists only of unique symbols\n";
+  };
 };
 
 struct Automata {
 private:
-  int start_state = 0;
+  int start_state = -1;
+  int total_amount_of_states = 0;
   std::vector<int> terminal;
+  std::vector<std::vector<Symbol>> determine;
   std::vector<Transition> transits;
   Alphabet al;
 public:
@@ -143,6 +190,10 @@ public:
 
   void set_start_state(int s) {
     start_state = s;
+  }
+
+  void set_total(int t) {
+    total_amount_of_states = t;
   }
 
   void read_transits(std::string &s) {
@@ -191,6 +242,7 @@ public:
         }
         t *= 10;
         t += (s[i] - '0');
+        t += (s[i] - '0');
         i++;
       }
       terminal.push_back(t);
@@ -207,7 +259,7 @@ public:
 
   void print_terminal(std::ofstream &fout) {
     fout << "Terminal states is: ";
-    for (const auto i: terminal) {
+    for (const auto i : terminal) {
       fout << i << " ";
     }
     fout << "\n";
@@ -221,11 +273,89 @@ public:
   }
 
   void print_au(std::ofstream &fout) {
+    fout << "Total amount of states is: " << total_amount_of_states << "\n";
     print_al(fout);
     fout << "\n";
     fout << "Start state is: " << start_state << '\n';
     print_terminal(fout);
     print_transits(fout);
+  }
+
+  void test_start_state(std::ofstream &fout) {
+    if (start_state >= 0 && start_state < total_amount_of_states) {
+      fout << "Automata has Start State\n";
+    }
+    else {
+      fout << "Start State is missing\n";
+    }
+  }
+
+  void test_alph(std::ofstream &fout) {
+    al.test_alph(fout);
+  }
+
+  void test_full(std::ofstream &fout) {
+    int sum = 0;
+    for (auto &t : transits) {
+      sum += t.cond_size();
+    }
+    if (al.alph_size() * total_amount_of_states == sum) {
+      fout << "Automata is full\n";
+    }
+    else {
+      fout << "Automata is not full\n";
+    }
+  }
+
+  void test_determine(std::ofstream &fout) {
+    determine.resize(total_amount_of_states);
+    for (auto & t : transits) {
+      for (auto & c : t.get_cond()) {
+        determine[t.get_begin()].push_back(c);
+      }
+    }
+    for (auto & v : determine) {
+      for (int i = 0; i < v.size(); i++) {
+        for (int j = i + 1; j < v.size(); j++) {
+          if (v[i] == v[j]) {
+            fout << "Automata is not determine\n";
+          }
+        }
+      }
+    }
+    fout << "Automata is determine\n";
+  }
+
+  void test_transits(std::ofstream &fout) {
+    for (auto t : transits) {
+      if (t.get_begin() < 0 || t.get_begin() >= total_amount_of_states || t.get_end() < 0 || t.get_end() >= total_amount_of_states) {
+        fout << "Some Transits has incorrect data\n";
+        return;
+      }
+      for (auto &c : t.get_cond()) {
+        bool b;
+        for (auto &s : al.get_alph()) {
+          if (c == s) {
+            b = false;
+            break;
+          }
+        }
+        if (b) {
+          fout << "Some Transits has incorrect data\n";
+          return;
+        }
+      }
+    }
+    fout << "All Transits are correct";
+  }
+
+  void test(std::ofstream &fout) {
+    test_start_state(fout);
+    fout << "Each state is unique\n";
+    test_alph(fout);
+    test_determine(fout);
+    test_full(fout);
+    test_transits(fout);
   }
 };
 
@@ -248,7 +378,6 @@ int main(int argc, char *argv[]) {
   std::ifstream fin(argv[1]);
   std::ofstream fout(std::string(argv[1]) + ".out");
   Automata au;
-  int state_num = 0;
   for (int i = 0; i < 5 && is_everything_ok; i++) {
     std::string s;
     std::getline(fin, s);
@@ -266,7 +395,7 @@ int main(int argc, char *argv[]) {
         break;
       }
       s = s.substr(9);
-      state_num = read_int(s);
+      au.set_total(read_int(s));
     }
     if (i == 2) {
       if (s.substr(0, 14) != "Start_state = ") {
@@ -296,8 +425,8 @@ int main(int argc, char *argv[]) {
 finish:
   if (is_everything_ok) {
     fout << "Automata is correct\n";
-    fout << "Total amount of states is: " << state_num << "\n";
     au.print_au(fout);
+    au.test(fout);
   }
   else {
     fout << "Incorrect input";
