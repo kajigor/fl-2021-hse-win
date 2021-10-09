652b53a56d0bf912654b28d838181f8c698a4234
Mikhail Petrochenkov <mishok2503@mail.ru>
2021-10-10 01:25:03 +0300

    more parser errors

--- parser/main.cpp
+++ parser/main.cpp
@@ -4,8 +4,6 @@
 #include <vector>
 #include <set>
 
-struct parse_exception { int line_num; };
-
 std::string serialize_vector(size_t v) {
     return std::to_string(v);
 }
@@ -65,13 +63,15 @@ auto serialize(const int q, const auto& words, const auto& states, const auto& t
 
 
 int main() {
-    auto read_line = [line_num = 0](auto& is, const char start_char) mutable {
+
+    int line_num = 0;
+    auto read_line = [&line_num](auto& is, const char start_char) mutable {
         ++line_num;
         std::string line;
         std::getline(is, line);
         auto start_str = start_char + std::string{": "};
         if (line.compare(0, 3, start_str)) {
-            throw parse_exception{line_num};
+            throw std::runtime_error("start characters must be \"" + start_str + "\"");
         }
         return line.substr(3);
     };
@@ -79,61 +79,95 @@ int main() {
     try {
         using std::cin;
 
-        std::stringstream ss(read_line(cin, 's')); //TODO: error if not (\w|\s)*
+        std::stringstream ss(read_line(cin, 's'));
 
         size_t n, m, q, t;
-        ss >> n >> m >> q >> t;
+        if (!(ss >> n >> m >> q >> t)) {
+            throw std::runtime_error("first line must contains 4 numbers");
+        }
 
         auto non_zero = [](const int n, const std::string& c) {
             if (n == 0) {
-                throw std::invalid_argument(c + " must be greater then zero");
+                throw std::runtime_error(c + " must be greater then zero");
             }
         };
         non_zero(n, "n");
         non_zero(m, "m");
         auto compare_with_n = [&n](const int q, const std::string& c) {
             if (q > n) {
-                throw std::invalid_argument(c + " must be not greater when n");
+                throw std::runtime_error(c + " must be not greater when n");
             }
         };
         compare_with_n(q + 1, "q + 1");
         compare_with_n(t, "t");
+
+        auto get_num_err = [](const size_t expected) {
+            return "expected " + std::to_string(expected) + " numbers";
+        };
+
+        auto ss_empty = [&ss, &get_num_err](const size_t expected) {
+            if (int t; ss >> t) {
+                throw std::runtime_error(get_num_err(expected));
+            }
+        };
+
+        auto check_word = [](const auto& word) {
+            char prev = '\0';
+            constexpr auto err_msg = "after \"\\\" must be \"\\\" or \"m\"";
+            for (char cur : word) {
+                if (prev == '\\' && cur != '\\' && cur != 'n') {
+                    throw std::runtime_error(err_msg);
+                }
+                prev = cur;
+            }
+            if (prev == '\\') {
+                throw std::runtime_error(err_msg);
+            }
+        };
 
         std::unordered_map<std::string, size_t> word_to_num;
         std::vector< std::vector<size_t> > next_state(n, std::vector<size_t>(m));
         std::set<size_t> terminals;
 
         for (size_t i=0; i < m; ++i) {
-            const auto& [it, is_inserted] = word_to_num.emplace(read_line(cin, 'w'), i); //TODO: change \\ to \ etc.
+            const auto& [it, is_inserted] = word_to_num.emplace(read_line(cin, 'w'), i);
             if (!is_inserted) {
-                throw std::invalid_argument("Repeated word: \"" + it->first + "\"");
+                throw std::runtime_error("Repeated word: \"" + it->first + "\"");
             }
+            check_word(it->first);
         }
+
+        auto read_num = [&ss, &get_num_err](const size_t expected) {
+            size_t x;
+            if (!(ss >> x)) {
+                throw std::runtime_error(get_num_err(expected));
+            }
+            return x;
+        };
 
         for (auto& i : next_state) {
             ss = std::stringstream(read_line(cin, 'n'));
             for (auto& j : i) {
-                ss >> j;
-                if (j >= n) {
-                    throw std::invalid_argument("States must be in 0..(n - 1)");
+                if ((j = read_num(m)) >= n) {
+                    throw std::runtime_error("States must be in 0..(n - 1)");
                 }
-            } //TODO: check for empty ss
+            }
+            ss_empty(m);
         }
 
         ss = std::stringstream(read_line(cin, 't'));
         for (size_t i=0; i < t; ++i) {
-            int t_state;
-            ss >> t_state; // TODO: check for repeating
-            terminals.insert(t_state);
-        } //TODO: check for empty ss
+            const auto& [it, is_inserted] = terminals.emplace(read_num(t));
+            if (!is_inserted) {
+                throw std::runtime_error("Repeated terminal state: " + std::to_string(*it));
+            }
+        }
+        ss_empty(t);
 
         std::cout << serialize(q, word_to_num, next_state, terminals);
-    } catch (parse_exception& e) {
-        std::cerr << "Can't parse " << e.line_num << " line\n";
+    } catch (std::runtime_error& e) {
+        std::cerr << "line " << std::to_string(line_num) << ": " << e.what() << '\n';
         return 1;
-    } catch (std::invalid_argument& e) {
-        std::cerr << e.what() << '\n';
-        return 2;
     }
 
     return 0;
