3ac437b5d935953f862d70661110ea7e6a817c99
arkady <arksap2002@gmail.com>
2021-10-10 04:37:45 +0300

    base done

--- solution/parser/main.py
+++ solution/parser/main.py
@@ -1,6 +1,7 @@
 import ply.lex as lex
 
-tokens = ['method', 'var', 'last_par', 'body_begin', 'body_end', 'skip', 'return', 'par']
+tokens = ['method', 'var_init', 'last_par', 'body_begin', 'body_end',
+          'skip', 'return', 'par', 'var', 'if', 'else', 'operator']
 file_out = open("output.dot", 'w')
 stack = ["v_0"]
 index_of_vertex = 1
@@ -15,20 +16,12 @@ def create_name(name):
     return result
 
 
-def create_method_vertex():
-    return create_name("Method")
-
-
-def create_type_vertex(name):
-    return create_name("Type=\"" + name + "\"")
+def create_type_vertex(type_):
+    return create_name("Type=\"" + type_ + "\"")
 
 
 def create_parameters_list_vertex():
     return create_name("Parameters list")
-
-
-def create_body_vertex():
-    return create_name("Body")
 
 
 def create_parameter_vertex():
@@ -39,16 +32,8 @@ def create_name_vertex(name):
     return create_name("Name=\"" + name + "\"")
 
 
-def create_var_vertex():
-    return create_name("Variable")
-
-
 def create_var_value_vertex(value):
     return create_name("Value=\"" + value + "\"")
-
-
-def create_return_vertex():
-    return create_name("Return")
 
 
 def get_type(s):
@@ -60,6 +45,18 @@ def get_type(s):
     return result
 
 
+def char_checking(ch):
+    if ch == '>':
+        return "&gt;"
+    if ch == '<':
+        return "&lt;"
+    if ch == '&':
+        return "&amp;"
+    if ch != ' ':
+        return ch
+    return ''
+
+
 def get_var_value(s):
     index = 0
     result = ""
@@ -69,8 +66,7 @@ def get_var_value(s):
     while index < len(s) and s[index] == ' ':
         index += 1
     while index < len(s) and s[index] != ';':
-        if s[index] != ' ':
-            result += s[index]
+        result += char_checking(s[index])
         index += 1
     return result
 
@@ -82,8 +78,8 @@ def get_name(s):
         index += 1
     while index < len(s) and s[index] == ' ':
         index += 1
-    while index < len(s) and (s[index] != ' ' and s[index] != '(' and s[index] != ',' and s[index] != ')'):
-        result += s[index]
+    while index < len(s) and s[index] != ' ' and s[index] != '(' and s[index] != ',' and s[index] != ')':
+        result += char_checking(s[index])
         index += 1
     return result
 
@@ -94,8 +90,36 @@ def get_return_value(s):
     while index < len(s) and s[index] != ' ':
         index += 1
     while index < len(s) and s[index] != ';':
-        if s[index] != ' ':
-            result += s[index]
+        result += char_checking(s[index])
+        index += 1
+    return result
+
+
+def get_if_value(s):
+    result = ""
+    index = 0
+    while index < len(s) and s[index] != '(':
+        index += 1
+    index += 1
+    while index < len(s) and s[index] != ')':
+        result += char_checking(s[index])
+        index += 1
+    return result
+
+
+def get_operator_name(s):
+    result = ""
+    index = 0
+    while index < len(s) and s[index] != ' ':  # type
+        index += 1
+    while index < len(s) and s[index] == ' ':
+        index += 1
+    while index < len(s) and s[index] != ' ':  # operator
+        index += 1
+    while index < len(s) and s[index] == ' ':
+        index += 1
+    while index < len(s) and s[index] != ' ' and s[index] != '(':
+        result += char_checking(s[index])
         index += 1
     return result
 
@@ -107,8 +131,8 @@ def init_vertex(vertex, s):
 
 
 def t_method(t):
-    r"""(int|int2|string)\s+\w*\("""
-    new_vertex = create_method_vertex()
+    r"""(int|int2|string)\s+\w*\s*\("""
+    new_vertex = create_name("Method")
     init_vertex(new_vertex, t.value)
     stack.append(new_vertex)
     stack.append(create_parameters_list_vertex())
@@ -118,16 +142,9 @@ def t_method(t):
 
 def t_body_begin(t):
     r"""\{"""
-    new_vertex = create_body_vertex()
+    new_vertex = create_name("Body")
     file_out.write(stack[-1] + to_arrow + new_vertex + "\n")
     stack.append(new_vertex)
-    return t
-
-
-def t_body_end(t):
-    r"""\}"""
-    stack.pop()
-    stack.pop()
     return t
 
 
@@ -147,9 +164,9 @@ def t_last_par(t):
     return t
 
 
-def t_var(t):
+def t_var_init(t):
     r"""(int|int2|string)\s+\w+\s*=\s*.*;"""
-    new_vertex = create_var_vertex()
+    new_vertex = create_name("Variable init")
     init_vertex(new_vertex, t.value)
     file_out.write(new_vertex + to_arrow + create_var_value_vertex(get_var_value(t.value)) + "\n")
     return t
@@ -157,9 +174,56 @@ def t_var(t):
 
 def t_return(t):
     r"""return.*;"""
-    new_vertex = create_return_vertex()
+    new_vertex = create_name("Return")
     file_out.write(stack[-1] + to_arrow + new_vertex + "\n")
     file_out.write(new_vertex + to_arrow + create_type_vertex(get_return_value(t.value)) + "\n")
+    return t
+
+
+def t_var(t):
+    r"""\w+\s*=\s*.*;"""
+    new_vertex = create_name("Variable assignment")
+    file_out.write(stack[-1] + to_arrow + new_vertex + "\n")
+    file_out.write(
+        new_vertex + to_arrow + create_name_vertex(get_type(t.value)) + "\n")  # get_type, because of line starting
+    file_out.write(new_vertex + to_arrow + create_var_value_vertex(get_var_value(t.value)) + "\n")
+    return t
+
+
+def t_if(t):
+    r"""if\s*\(.*\)"""
+    new_vertex = create_name("If statement")
+    file_out.write(stack[-1] + to_arrow + new_vertex + "\n")
+    file_out.write(new_vertex + to_arrow + create_name("Condition=\"" + get_if_value(t.value) + "\"") + "\n")
+    stack.append(new_vertex)
+    return t
+
+
+def t_else(t):
+    r"""\}\s*else"""
+    new_vertex = create_name("Else statement")
+    stack.pop()
+    file_out.write(stack[-1] + to_arrow + new_vertex + "\n")
+    stack.append(new_vertex)
+    return t
+
+
+def t_operator(t):
+    r"""(int|int2|string)\s+operator\s+.+\s*\("""
+    new_vertex = create_name("Operator")
+    file_out.write(stack[-1] + to_arrow + new_vertex + "\n")
+    file_out.write(new_vertex + to_arrow + create_name_vertex(get_operator_name(t.value)) + "\n")
+    file_out.write(new_vertex + to_arrow + create_type_vertex(get_type(t.value)) + "\n")
+    stack.append(new_vertex)
+    stack.append(create_parameters_list_vertex())
+    file_out.write(stack[-2] + to_arrow + stack[-1] + "\n")
+    return t
+
+
+def t_body_end(t):
+    r"""\}"""
+    stack.pop()  # Body exit
+    stack.pop()  # Statement exit
     return t
 
 
@@ -192,3 +256,4 @@ def main():
 
 
 main()
+
