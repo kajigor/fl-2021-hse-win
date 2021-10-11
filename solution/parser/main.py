11373e64d03008ca9088083968b57b65c4b23900
arkady <arksap2002@gmail.com>
2021-10-12 02:11:55 +0300

    expression progress begin

--- solution/parser/main.py
+++ solution/parser/main.py
@@ -6,6 +6,17 @@ file_out = open("output.dot", 'w')
 stack = ["v_0"]
 index_of_vertex = 1
 to_arrow = " -> "
+operators = {"^": [2, True], "--": [3, True], "*": [4, False],
+             "/": [4, False], "+": [5, False], "-": [5, False], "==": [6, True], "/=": [6, True], "<=": [6, True],
+             "<": [6, True], ">=": [6, True], ">": [6, True], "!": [7, True], "&&": [8, True],
+             "||": [9, True], "=": [10, True]}  # [prio, is_not_left_associativity]
+
+
+def error(ch):
+    file_out.write("}\n")
+    file_out.close()
+    print("Illegal character '%s'" % ch)
+    exit()
 
 
 def create_name(name):
@@ -32,8 +43,8 @@ def create_name_vertex(name):
     return create_name("Name=\"" + name + "\"")
 
 
-def create_value_vertex(value):
-    return create_name("Value=\"" + value + "\"")
+def create_value_vertex():
+    return create_name("Value")
 
 
 def index_checking(s, index, arr):
@@ -74,7 +85,7 @@ def get_var_value(s):
     index += 1
     while index < len(s) and s[index] == ' ':
         index += 1
-    while index_checking(s, index, {';', ')'}):
+    while index_checking(s, index, {';', ']'}):
         result += char_checking(s[index])
         index += 1
     return result
@@ -165,16 +176,138 @@ def get_condition(s):
     result = ""
     index = 0
     while index < len(s):
-        if s[index] != ' ' and s[index] != ')' and s[index] != ';':
-            result += char_checking(s[index])
+        if s[index] != ' ' and s[index] != ']' and s[index] != ';':
+            result += s[index]
         index += 1
-    return result
+    return expression_process(result)
 
 
 def init_vertex(vertex, s):
     file_out.write(stack[-1] + to_arrow + vertex + "\n")
     file_out.write(vertex + to_arrow + create_name_vertex(get_name(s)) + "\n")
     file_out.write(vertex + to_arrow + create_type_vertex(get_type(s)) + "\n")
+
+
+def init_value_tree(expression, new_vertex):
+    value_vertex = create_value_vertex()
+    file_out.write(new_vertex + to_arrow + value_vertex + "\n")
+    stack.append(value_vertex)
+    file_out.write(stack[-1] + to_arrow + expression_process(expression) + "\n")
+    stack.pop()
+
+
+def is_digit(ch):
+    return '0' <= ch <= '9'
+
+
+def is_letter(ch):
+    return 'a' <= ch <= 'z' or 'A' <= ch <= 'Z'
+
+
+def expression_process(s):
+    arr = []
+    index = 0
+    while index < len(s):
+        if s[index] == ' ':
+            # space
+            index += 1
+        elif s[index] == '(':
+            # brackets
+            bal = 1
+            index += 1
+            while index < len(s) and bal != 0:
+                if s[index] == '(':
+                    bal += 1
+                if s[index] == ')':
+                    bal -= 1
+                index += 1
+        elif is_digit(s[index]):
+            # number
+            while index < len(s) and is_digit(s[index]):
+                index += 1
+        elif is_letter(s[index]):
+            # word
+            while index < len(s) and (is_letter(s[index]) or is_digit(s[index]) or s[index] == '_'):
+                index += 1
+        else:
+            # operator
+            result = ""
+            start = index
+            while index < len(s) and (s[index] != ' ' and s[index] != '(' and s[index] != ')'
+                                      and not is_letter(s[index]) and not is_digit(s[index])):
+                result += s[index]
+                index += 1
+            if result in operators:
+                if len(arr) == 0 or operators[result][0] > arr[0][0] or (
+                        operators[result][0] == arr[0][0] and not operators[result][1]):
+                    arr = [operators[result], [start, index]]
+            else:
+                error(s[index])
+    if arr:
+        # external operator found
+        cur_operator = s[arr[1][0]:arr[1][1]]
+        for i in range(0, len(cur_operator)):
+            if i == len(cur_operator) - 1:
+                cur_operator = cur_operator[:i:] + char_checking(cur_operator[i])
+            elif i == 0:
+                cur_operator = char_checking(cur_operator[i]) + cur_operator[i + 1::]
+            else:
+                cur_operator = cur_operator[:i:] + char_checking(cur_operator[i]) + cur_operator[i + 1::]
+        new_vertex = create_name("Operator=\"" + cur_operator + "\"")
+        stack.append(new_vertex)
+        file_out.write(stack[-1] + to_arrow + expression_process(s[0:arr[1][0]:]) + "\n")
+        file_out.write(stack[-1] + to_arrow + expression_process(s[arr[1][1]::]) + "\n")
+        stack.pop()
+        return new_vertex
+    else:
+        # external operator not found
+        index = 0
+        if s == "":
+            # empty
+            return create_name("")
+        if is_digit(s[0]):
+            # alone number
+            return create_name("Number=\"" + s + "\"")
+        if len(s) >= 2 and s[0] == '(' and s[len(s) - 1] == ')':
+            # expression in brackets
+            new_vertex = create_name("Brackets")
+            stack.append(new_vertex)
+            file_out.write(stack[-1] + to_arrow + expression_process(s[1:len(s) - 1:]) + "\n")
+            stack.pop()
+            return new_vertex
+        if len(s) > 2 and is_letter(s[index]):
+            # alone variable or method
+            name = ""
+            while index < len(s) and (is_letter(s[index]) or is_digit(s[index]) or s[index] == '_'):
+                name += char_checking(s[index])
+                index += 1
+            if len(s) == index:
+                # alone variable
+                return create_name("Variable=\"" + name + "\"")
+            if s[index] != '(':
+                error(s[index])
+            # alone method
+            new_vertex = create_name("Method assignment")
+            stack.append(new_vertex)
+            file_out.write(stack[-1] + to_arrow + create_name_vertex(name) + "\n")
+            stack.append(create_parameters_list_vertex())
+            file_out.write(stack[-2] + to_arrow + stack[-1] + "\n")
+            index += 1
+            while index < len(s) and s[index] != ')':
+                result = ""
+                while index < len(s) and s[index] != ',' and s[index] != ')':
+                    result += s[index]
+                    index += 1
+                index += 1
+                parameter_vertex = create_parameter_vertex()
+                file_out.write(stack[-1] + to_arrow + parameter_vertex + "\n")
+                stack.append(parameter_vertex)
+                file_out.write(stack[-1] + to_arrow + expression_process(result) + "\n")
+                stack.pop()
+            stack.pop()
+            stack.pop()
+            return new_vertex
+        return create_name("Variable=\"" + s + "\"")
 
 
 def t_method(t):
@@ -215,7 +348,7 @@ def t_var_init(t):
     r"""(int|int2|string)\s+\w+\s*=\s*[^;]*;"""
     new_vertex = create_name("Variable init")
     init_vertex(new_vertex, t.value)
-    file_out.write(new_vertex + to_arrow + create_value_vertex(get_var_value(t.value)) + "\n")
+    init_value_tree(get_var_value(t.value), new_vertex)
     return t
 
 
@@ -223,17 +356,17 @@ def t_return(t):
     r"""return.*;"""
     new_vertex = create_name("Return")
     file_out.write(stack[-1] + to_arrow + new_vertex + "\n")
-    file_out.write(new_vertex + to_arrow + create_value_vertex(get_return_value(t.value)) + "\n")
+    init_value_tree(get_return_value(t.value), new_vertex)
     return t
 
 
 def t_var(t):
-    r"""\w+\s*=\s*.*(;|\))"""
+    r"""\w*\s*[^=]=[^=]\s*.*(;|\])"""
     new_vertex = create_name("Variable assignment")
     file_out.write(stack[-1] + to_arrow + new_vertex + "\n")
     file_out.write(
         new_vertex + to_arrow + create_name_vertex(get_type(t.value)) + "\n")  # get_type, because of line starting
-    file_out.write(new_vertex + to_arrow + create_value_vertex(get_var_value(t.value)) + "\n")
+    init_value_tree(get_var_value(t.value), new_vertex)
     return t
 
 
@@ -241,7 +374,6 @@ def t_if(t):
     r"""if\s*\("""
     new_vertex = create_name("If statement")
     file_out.write(stack[-1] + to_arrow + new_vertex + "\n")
-    # file_out.write(new_vertex + to_arrow + create_name("Condition=\"" + get_if_value(t.value) + "\"") + "\n")
     stack.append(new_vertex)
     return t
 
@@ -263,6 +395,7 @@ def t_operator(t):
     file_out.write(new_vertex + to_arrow + create_type_vertex(get_type(t.value)) + "\n")
     file_out.write(new_vertex + to_arrow + create_name("Priority=\"" + get_prio(t.value) + "\"") + "\n")
     file_out.write(new_vertex + to_arrow + create_name("Associativity=\"" + get_assoc(t.value) + "\"") + "\n")
+    operators[get_operator_name(t.value)] = [int(get_prio(t.value)), get_assoc(t.value) != 1]
     stack.append(new_vertex)
     stack.append(create_parameters_list_vertex())
     file_out.write(stack[-2] + to_arrow + stack[-1] + "\n")
@@ -270,7 +403,7 @@ def t_operator(t):
 
 
 def t_for(t):
-    r"""for\s*\("""
+    r"""for\s*\["""
     new_vertex = create_name("For statement")
     file_out.write(stack[-1] + to_arrow + new_vertex + "\n")
     stack.append(new_vertex)
@@ -285,21 +418,22 @@ def t_body_end(t):
 
 
 def t_skip(t):
-    r"""(\s+|\n)"""
+    r"""(\s+|\n|\]|\))"""
     return t
 
 
 def t_condition(t):
-    r"""[^\;&^\)]+(\;|\))"""
-    file_out.write(stack[-1] + to_arrow + create_name("Condition=\"" + get_condition(t.value) + "\"") + "\n")
+    r"""[^\;&^\]&^\n]+(\)|\]|\;)"""  # TODO think, how to use ^
+    new_vertex = create_name("Condition")
+    file_out.write(stack[-1] + to_arrow + new_vertex + "\n")
+    stack.append(new_vertex)
+    file_out.write(stack[-1] + to_arrow + get_condition(t.value[:len(t.value) - 1:]) + "\n")
+    stack.pop()
     return t
 
 
 def t_error(t):
-    file_out.write("}\n")
-    file_out.close()
-    print("Illegal character '%s'" % t.value[0])
-    exit()
+    error(t.value[0])
     return t
 
 
