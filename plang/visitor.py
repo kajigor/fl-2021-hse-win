b32c0e778e092e57eeee0164702803ee6ae64e04
stepan <stepan@konstantinov.pl>
2021-10-21 03:36:59 +0300

    Minor

--- plang/visitor.py
+++ plang/visitor.py
@@ -3,7 +3,8 @@ def main():
     file = open('programs/program1.p', 'r')
     program = file.read().strip()
     file.close()
-    
+
 
 if __name__ == '__main__':
+    # antlr4 -Dlanguage=Python3 -visitor plang.g4
     main()
