73088d8c004f12a30631d4ff42a8ffe37030d0cc
stepan <stepan@konstantinov.pl>
2021-10-22 16:41:51 +0300

    Tests id fix

--- plang/testing/Tester.py
+++ plang/testing/Tester.py
@@ -2,21 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import os
+import time
 
 from plang.kernel.Parser import Parser
 from Generator import Generator
-from Counter import Counter
 
 
 class Tester:
 	def __init__(self, seed=1337):
 		self.generator = Generator.setSeed(seed)
 
-	def saveTestLog(self, dirname, program, out):
-		Counter.inc()
-		directory = './logs/' + dirname + '/' + str(Counter.value()) + '/'
+	def saveTestLog(self, test_id, dirname, program, out):
+		directory = './logs/' + dirname + '/' + str(test_id) + '/'
 		os.mkdir(directory)
-		path = directory + 'program' + str(Counter.value()) + '.p'
+		path = directory + 'program' + str(test_id) + '.p'
 		with open(path, 'w') as file:
 			file.write(program)
 		with open(path + '.out', 'w') as file:
@@ -32,9 +31,10 @@ class Tester:
 		return 'incorrect'
 
 	def doTest(self, program, correctness):
+		test_id = hex(int(str(time.time()).replace('.', '')))[2:]
 		parser = Parser(program)
 		situation = self.getTestResultName(correctness, parser.parse(stderr=False, saveErrors=True))
-		self.saveTestLog(situation, program, parser.getResult())
+		self.saveTestLog(test_id, situation, program, parser.getResult())
 		return situation
 
 	def doTests(self, tests):
